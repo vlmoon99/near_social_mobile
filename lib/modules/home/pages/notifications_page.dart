b0032c9ba87950c21938e4325db64c0598ab9473
ArturLevchuk <levchukarturr@gmail.com>
2024-06-11 14:35:22 +0300

    added resfreshing for notifications page

--- lib/modules/home/pages/notifications_page.dart
+++ lib/modules/home/pages/notifications_page.dart
@@ -19,6 +19,7 @@ class NotificationsPage extends StatefulWidget {
 
 class _NotificationsPageState extends State<NotificationsPage> {
   bool allNotificationsLoaded = false;
+  bool moreNotificationsLoading = false;
 
   String getFullActionDescription(Notification notification) {
     switch (notification.notificationType.type) {
@@ -71,85 +72,108 @@ class _NotificationsPageState extends State<NotificationsPage> {
             child: SpinnerLoadingIndicator(),
           );
         }
-        return ListView.builder(
-          itemBuilder: (context, index) {
-            if (index == notificationsController.state.notifications.length) {
-              return RPadding(
-                padding: const EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: allNotificationsLoaded
-                      ? null
-                      : () {
-                          HapticFeedback.lightImpact();
-                          final AuthController authController =
-                              Modular.get<AuthController>();
-                          final accountId = authController.state.accountId;
-                          notificationsController
-                              .loadMoreNotifications(
-                            accountId: accountId,
-                          )
-                              .then((moreNotifications) {
-                            if (moreNotifications.isEmpty) {
-                              setState(() {
-                                allNotificationsLoaded = true;
-                              });
-                            }
-                          });
-                        },
-                  child: allNotificationsLoaded
-                      ? const Text("No more notifications")
-                      : const Text("Load more notifications"),
-                ),
-              );
-            }
-            final notification =
-                notificationsController.state.notifications[index];
-            return ListTile(
-              leading: Container(
-                width: 40.w,
-                height: 40.w,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                ),
-                clipBehavior: Clip.antiAlias,
-                child: NearNetworkImage(
-                  imageUrl: notification.authorInfo.profileImageLink,
-                  placeholder: Image.asset(
-                    NearAssets.standartAvatar,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              title: Text.rich(
-                TextSpan(text: notification.authorInfo.name, children: [
-                  TextSpan(
-                    text: " @${notification.authorInfo.accountId}",
-                    style: const TextStyle(color: Colors.grey),
-                  )
-                ]),
-              ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    getFullActionDescription(notification),
-                    style: TextStyle(fontSize: 16.sp),
-                  ),
-                  SizedBox(width: 10.w),
-                  Text(
-                    DateFormat('hh:mm a MMM dd, yyyy')
-                        .format(notification.date),
-                    style: TextStyle(
-                      color: Colors.grey.shade600,
-                      fontSize: 12.sp,
-                    ),
-                  )
-                ],
-              ),
+        return RefreshIndicator.adaptive(
+          onRefresh: () async {
+            final AuthController authController = Modular.get<AuthController>();
+            final accountId = authController.state.accountId;
+            return notificationsController.loadNotifications(
+              accountId: accountId,
+              loadingIndicator: false,
             );
           },
-          itemCount: notificationsController.state.notifications.length + 1,
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              if (index == notificationsController.state.notifications.length) {
+                return RPadding(
+                  padding: const EdgeInsets.all(20),
+                  child: moreNotificationsLoading
+                      ? const Center(child: SpinnerLoadingIndicator())
+                      : ElevatedButton(
+                          onPressed: allNotificationsLoaded
+                              ? null
+                              : () async {
+                                  HapticFeedback.lightImpact();
+                                  try {
+                                    setState(() {
+                                      moreNotificationsLoading = true;
+                                    });
+                                    final AuthController authController =
+                                        Modular.get<AuthController>();
+                                    final accountId =
+                                        authController.state.accountId;
+                                    final moreNotifications =
+                                        await notificationsController
+                                            .loadMoreNotifications(
+                                      accountId: accountId,
+                                    );
+                                    if (moreNotifications.isEmpty) {
+                                      setState(() {
+                                        allNotificationsLoaded = true;
+                                      });
+                                    }
+                                  } catch (err) {
+                                    rethrow;
+                                  } finally {
+                                    setState(() {
+                                      moreNotificationsLoading = false;
+                                    });
+                                  }
+                                },
+                          child: allNotificationsLoaded
+                              ? const Text("No more notifications")
+                              : const Text("Load more notifications"),
+                        ),
+                );
+              }
+              final notification =
+                  notificationsController.state.notifications[index];
+              return ListTile(
+                leading: Container(
+                  width: 40.w,
+                  height: 40.w,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  clipBehavior: Clip.antiAlias,
+                  child: NearNetworkImage(
+                    imageUrl: notification.authorInfo.profileImageLink,
+                    placeholder: Image.asset(
+                      NearAssets.standartAvatar,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                title: Text.rich(
+                  TextSpan(text: notification.authorInfo.name, children: [
+                    TextSpan(
+                      text: " @${notification.authorInfo.accountId}",
+                      style: const TextStyle(color: Colors.grey),
+                    )
+                  ]),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      getFullActionDescription(notification),
+                      style: TextStyle(fontSize: 16.sp),
+                    ),
+                    SizedBox(width: 10.w),
+                    Text(
+                      DateFormat('hh:mm a MMM dd, yyyy')
+                          .format(notification.date),
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontSize: 12.sp,
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+            itemCount: notificationsController.state.notifications.length + 1,
+          ),
         );
       },
     );
