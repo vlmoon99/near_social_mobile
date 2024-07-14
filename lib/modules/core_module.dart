1e9e3b8f1042172c4d24835a4d54671f539cbb94
ArturLevchuk <levchukarturr@gmail.com>
2024-07-14 12:38:28 +0300

    refactoring custom near network client

--- lib/modules/core_module.dart
+++ lib/modules/core_module.dart
@@ -1,13 +1,11 @@
-import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:flutterchain/flutterchain_lib/constants/chains/near_blockchain_network_urls.dart';
 import 'package:flutterchain/flutterchain_lib/network/chains/near_rpc_client.dart';
 import 'package:flutterchain/flutterchain_lib/services/chains/near_blockchain_service.dart';
 import 'package:near_social_mobile/config/constants.dart';
 import 'package:flutterchain/flutterchain_lib/services/core/js_engines/platforms_implementations/webview_js_engine.dart';
-import 'package:near_social_mobile/services/dio_interceptors/retry_with_change_base_url.dart';
+import 'package:near_social_mobile/network/near_custom_client.dart';
 
 class CoreModule extends Module {
   @override
@@ -31,20 +29,3 @@ class CoreModule extends Module {
   }
 }
 
-class CustomNearNetworkClient extends NearNetworkClient {
-  CustomNearNetworkClient({required super.baseUrl, required super.dio}) {
-    dio.interceptors.clear();
-    dio.interceptors.addAll(
-      [
-        RetryInterceptorWithSecondaryLink(
-          dio: dio,
-          retries: 8,
-          logPrint: log,
-          retryDelays: [const Duration(seconds: 1)],
-          primaryLink: NearUrls.blockchainRpc,
-          secondaryLink: NearBlockChainNetworkUrls.listOfUrls.elementAt(1),
-        )
-      ],
-    );
-  }
-}
