4b7d274ea8172dada48bc2cfcd9d9e610e439065
ArturLevchuk <levchukarturr@gmail.com>
2024-08-16 02:18:25 +0300

    fixed issue with showing svg

--- lib/shared_widgets/near_network_image.dart
+++ lib/shared_widgets/near_network_image.dart
@@ -1,6 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:near_social_mobile/shared_widgets/spinner_loading_indicator.dart';
 
 class NearNetworkImage extends StatelessWidget {
@@ -17,20 +20,36 @@ class NearNetworkImage extends StatelessWidget {
   final Widget? placeholder;
   final BoxFit boxFit;
 
+  final httpHeaders = const {"Referer": "https://near.social/"};
+
   @override
   Widget build(BuildContext context) {
     return CachedNetworkImage(
       imageUrl: imageUrl,
-      httpHeaders: const {"Referer": "https://near.social/"},
+      httpHeaders: httpHeaders,
       fit: boxFit,
-      errorWidget: (context, error, stackTrace) =>
-          errorPlaceholder ??
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.broken_image),
-            ],
-          ),
+      errorWidget: (context, error, stackTrace) {
+        if (stackTrace.toString().contains("Invalid image data")) {
+          return SvgPictureSupport(
+            imageUrl: imageUrl,
+            headers: httpHeaders,
+            placeholder: errorPlaceholder ??
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.broken_image),
+                  ],
+                ),
+          );
+        }
+        return errorPlaceholder ??
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.broken_image),
+              ],
+            );
+      },
       placeholder: (context, url) =>
           placeholder ??
           SizedBox(
@@ -42,6 +61,52 @@ class NearNetworkImage extends StatelessWidget {
               ],
             ),
           ),
+    );
+  }
+}
+
+class SvgPictureSupport extends StatelessWidget {
+  const SvgPictureSupport({
+    super.key,
+    required this.imageUrl,
+    required this.headers,
+    required this.placeholder,
+  });
+
+  final String imageUrl;
+  final Map<String, String>? headers;
+  final Widget placeholder;
+
+  Future<Uint8List> loadAsset() async {
+    try {
+      final file =
+          await DefaultCacheManager().getSingleFile(imageUrl, headers: headers);
+      if (file.path.endsWith(".svg")) {
+        return file.readAsBytesSync();
+      } else {
+        return Uint8List.fromList([]);
+      }
+    } catch (err) {
+      return Uint8List.fromList([]);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Uint8List>(
+      future: loadAsset(),
+      builder: (context, snapshot) {
+        return AnimatedSwitcher(
+          duration: const Duration(
+            milliseconds: 500,
+          ),
+          switchInCurve: Curves.easeIn,
+          switchOutCurve: Curves.easeOut,
+          child: snapshot.data != null && snapshot.data!.isNotEmpty
+              ? SvgPicture.memory(snapshot.data!)
+              : placeholder,
+        );
+      },
     );
   }
 }
