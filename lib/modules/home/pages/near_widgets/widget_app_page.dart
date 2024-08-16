b6f4f8af0a27a512d2864700da2787bbcbba687c
ArturLevchuk <levchukarturr@gmail.com>
2024-08-16 13:24:12 +0300

    added error messages showing for near widgets

--- lib/modules/home/pages/near_widgets/widget_app_page.dart
+++ lib/modules/home/pages/near_widgets/widget_app_page.dart
@@ -1,6 +1,9 @@
 import 'package:bos_gateway_viewer/bos_gateway_viewer.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:near_social_mobile/exceptions/exceptions.dart';
 import 'package:near_social_mobile/modules/home/apis/models/private_key_info.dart';
 import 'package:near_social_mobile/shared_widgets/custom_button.dart';
 
@@ -79,13 +82,28 @@ class NearWidget extends StatelessWidget {
     // });
     return Scaffold(
       body: SafeArea(
-        child: BosGatewayWidget(
-          widgetSettings: nearWidgetSetupCredentials.widgetSettings,
-          nearAuthCreds: NearAuthCreds(
-            network: nearWidgetSetupCredentials.network,
-            accountId: nearWidgetSetupCredentials.privateKeyInfo.publicKey,
-            privateKey: nearWidgetSetupCredentials.privateKeyInfo.privateKey,
-          ),
+        child: Stack(
+          children: [
+            BosGatewayWidget(
+              widgetSettings: nearWidgetSetupCredentials.widgetSettings,
+              nearAuthCreds: NearAuthCreds(
+                network: nearWidgetSetupCredentials.network,
+                accountId: nearWidgetSetupCredentials.privateKeyInfo.publicKey,
+                privateKey:
+                    nearWidgetSetupCredentials.privateKeyInfo.privateKey,
+              ),
+              onError: (errorMessage) {
+                if (!kIsWeb && !errorMessage.contains("TypeError")) {
+                  Modular.get<Catcher>().exceptionsHandler.add(
+                        AppExceptions(
+                          messageForUser: errorMessage,
+                          messageForDev: errorMessage,
+                        ),
+                      );
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
